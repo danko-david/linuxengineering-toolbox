fe13f92a5033977dc8f286cd1684c36080bc629b
Dankó Dávid <info@dankodavid.hu>
2020-03-24 14:48:52 +0100

    fix target image name

--- docker_builds/template_systems/basic_web/build.sh
+++ docker_builds/template_systems/basic_web/build.sh
@@ -7,5 +7,5 @@ cd ../../..
 #compile the `lxe_basic_server` image which is required by this image
 ./docker_builds/template_systems/basic_server/build.sh
 
-docker image build -t lxe_basic_server -f ./docker_builds/template_systems/basic_web/Dockerfile .
+docker image build -t lxe_basic_web -f ./docker_builds/template_systems/basic_web/Dockerfile .
 
