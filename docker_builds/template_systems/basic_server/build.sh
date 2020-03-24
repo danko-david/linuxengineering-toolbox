9d4a24967081ee1b080c04cf5e193a649641b268
Dankó Dávid <info@dankodavid.hu>
2020-03-24 14:55:51 +0100

    build only if not exists yet

--- docker_builds/template_systems/basic_server/build.sh
+++ docker_builds/template_systems/basic_server/build.sh
@@ -6,6 +6,8 @@ cd "$(dirname `readlink -f "$0"`)"
 cd ../../..
 
 #compile the `lxc_basic` image which is required by this image
-./docker_builds/template_systems/basic/build.sh
+if [[ "$(docker images -q lxe_basic 2> /dev/null)" == "" ]]; then
+	./docker_builds/template_systems/basic/build.sh
+fi
 
 docker image build -t lxe_basic_server -f ./docker_builds/template_systems/basic_server/Dockerfile .
--- docker_builds/template_systems/basic_web/build.sh
+++ docker_builds/template_systems/basic_web/build.sh
@@ -5,7 +5,9 @@ cd "$(dirname `readlink -f "$0"`)"
 cd ../../..
 
 #compile the `lxe_basic_server` image which is required by this image
-./docker_builds/template_systems/basic_server/build.sh
+if [[ "$(docker images -q lxe_basic_server 2> /dev/null)" == "" ]]; then
+	./docker_builds/template_systems/basic_server/build.sh
+fi
 
 docker image build -t lxe_basic_web -f ./docker_builds/template_systems/basic_web/Dockerfile .
 
